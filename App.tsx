c3df38ee51705d7afff7691f7bc98b4f1c14ecf4
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-07-27 17:18:58 +0500

    #6 What is JSX

--- App.tsx
+++ App.tsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 import { Text, View, Button } from 'react-native';
 function App(): JSX.Element {
+  let name = 'Ahmad';
+  let email = 'ahmad@gmail.com';
+  let age = 25;
+  function fruit(): React.ReactNode {
+    return 'apple';
+  }
+
   return (
     <View>
       <Text style={{ fontSize: 30 }}>React Native</Text>
       <Button title="Press Here" />
       <Text style={{ fontSize: 30 }}>Another Text</Text>
       <Button title="Press New Button" />
+      <Text style={{ fontSize: 30 }}>{name}</Text>
+      <Text style={{ fontSize: 30 }}>{email}</Text>
+      <Text style={{ fontSize: 30 }}>{age}</Text>
+      <Text style={{ fontSize: 30 }}>{fruit()}</Text>
+      <Text style={{ fontSize: 30 }}>{5 + 5}</Text>
     </View>
   );
 }
