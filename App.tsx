12f0819f716341235c0253fd6d1a824fdfecfec2
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-07-28 15:22:01 +0500

    #9 State in react native | State vs Variables

--- App.tsx
+++ App.tsx
@@ -1,35 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { Text, View, Button } from 'react-native';
-import CompanyData from './components/CompanyData';
+import { Button, Text, View } from 'react-native';
 function App(): JSX.Element {
-  const fruit = (value) => {
-    console.warn(value);
+  const [name, setName] = useState('Ali');
+  const changeName = () => {
+    setName('Ahmad');
   }
-
-
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Components</Text>
-      <UserData />
-      <CompanyData />
-      {/* <Button title="with ()" color={'red'} onPress={fruit()} /> */}
-      <Button title="without call () " color={'green'} onPress={fruit} />
-      <Button
-        title="with arrow () to pass parms"
-        onPress={() => fruit('apple')}
-      />
+      <Text style={{ fontSize: 30 }}>State in react-native</Text>
+      <Text style={{ fontSize: 30 }}>{name}</Text>
+      <Button title='Chnage Name' onPress={changeName} />
     </View>
   );
 }
-const UserData = () => {
-  return (
-    <View>
-      <Text style={{ fontSize: 30 }}>Name: Ahmad Ali</Text>
-      <Text style={{ fontSize: 30 }}>Age: 25</Text>
-      <Text style={{ fontSize: 30 }}>Email: ahmad@admin.com</Text>
-    </View>
-  );
-};
 
 export default App;
