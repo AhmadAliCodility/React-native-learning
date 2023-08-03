82134c1a2f7ca435dbb884054632939d86ccdb86
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-08-03 17:05:58 +0500

    #13 Basic Form in React-Native | form with state

--- App.tsx
+++ App.tsx
@@ -3,10 +3,25 @@ import React, { useState } from 'react';
 import { Text, StyleSheet, View, TextInput, Button } from 'react-native';
 function App(): JSX.Element {
   const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [display, setDisplay] = useState(false);
+
+  const clear = () => {
+    setName('');
+    setEmail('');
+    setPassword('');
+  };
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>Handle Text Input</Text>
-      <Text style={{ fontSize: 30 }}>Your Name is: {name}</Text>
+      <Text style={{ fontSize: 30 }}>Simple Form in React</Text>
+      {display ? (
+        <View>
+          <Text style={{ fontSize: 20 }}>Your Name: {name}</Text>
+          <Text style={{ fontSize: 20 }}>Your email: {email}</Text>
+          <Text style={{ fontSize: 20 }}>Your password: {password}</Text>
+        </View>
+      ) : null}
       <TextInput
         style={styles.textInput}
         placeholder="Enter name"
@@ -14,7 +29,29 @@ function App(): JSX.Element {
         // onChangeText={setName}
         onChangeText={text => setName(text)}
       />
-      <Button title="Clear Input" onPress={() => setName('')} />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Enter email"
+        value={email}
+        // onChangeText={setName}
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        style={styles.textInput}
+        secureTextEntry={true}
+        placeholder="Enter password"
+        value={password}
+        // onChangeText={setName}
+        onChangeText={text => setPassword(text)}
+      />
+      <View style={{ marginBottom:10 }}>
+        <Button
+          title="Submit"
+          color={'green'}
+          onPress={() => setDisplay(true)}
+        />
+      </View>
+      <Button title="Clear Input" onPress={clear} />
     </View>
   );
 }
