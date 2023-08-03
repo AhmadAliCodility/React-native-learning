bae6dc3116969623174598e9a5b296cd4cfc7e6c
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-08-03 15:39:09 +0500

    10 Props in react native

--- App.tsx
+++ App.tsx
@@ -1,16 +1,22 @@
 import React, { useState } from 'react';
 
 import { Button, Text, View } from 'react-native';
+
 function App(): JSX.Element {
-  const [name, setName] = useState('Ali');
-  const changeName = () => {
-    setName('Ahmad');
-  }
+  const [name, setName] = useState('AHmad');
   return (
     <View>
-      <Text style={{ fontSize: 30 }}>State in react-native</Text>
-      <Text style={{ fontSize: 30 }}>{name}</Text>
-      <Button title='Chnage Name' onPress={changeName} />
+      <Text style={{ fontSize: 30 }}>Propss in react-native</Text>
+      <User name={name} />
+      <Button title="Click here" onPress={() => setName('Ali')} />
+    </View>
+  );
+}
+function User(props): JSX.Element {
+  console.warn(props)
+  return (
+    <View>
+      <Text style={{ fontSize: 30 }}>Child Component {props.name}</Text>
     </View>
   );
 }
