6c509c370e01d5b2d1c86811861d52a1c6013986
Ahmad Ali <ahmed.ali@codilitysolutions.com>
2023-08-04 16:45:31 +0500

    #15 List with map function | without flatlist

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { Text, View, FlatList } from 'react-native';
+import { ScrollView, Text, View } from 'react-native';
 function App(): JSX.Element {
-  const DATA = [
+  const users = [
     {
       id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
       title: 'First Item',
@@ -15,17 +15,34 @@ function App(): JSX.Element {
       id: '58694a0f-3da1-471f-bd96-145571e29d72',
       title: 'Third Item',
     },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Fourth Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Fifth Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Third Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Fourth Item',
+    },
+    {
+      id: '58694a0f-3da1-471f-bd96-145571e29d72',
+      title: 'Fifth Item',
+    },
   ];
 
   return (
     <View>
       <Text style={{ fontSize: 30 }}>List with Flat Component</Text>
-      <FlatList
-        data={DATA}
-        renderItem={({item}) => <Text style={{ fontSize: 30 }}>{item.title}</Text>}
-        keyExtractor={item => item.id}
-      />
-
+      <ScrollView>
+        {users.map(user => <Text>{user.title}</Text>)}
+      </ScrollView>
     </View>
   );
 }
